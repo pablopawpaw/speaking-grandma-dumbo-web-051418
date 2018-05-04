e224f46e37b73aa2813ecf23d990718dd833d84f
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-05-04 21:30:59 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,9 @@
 def speak_to_grandma(statement)
   if statement == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif statement == statement.downcase
-    return "HUH?! SPEAK UP, SONNY!"
   elsif statement == statement.upcase
     return "NO, NOT SINCE 1938!"
   else 
-    return "Error"
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
